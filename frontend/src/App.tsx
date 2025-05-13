cab7e4a601df812b65198311f8f208a0b4675558
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-13 14:51:27 -0300

    Feat: Initializing 'frontend' and 'TailwindCSS'

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='h-screen w-full flex items-center justify-center'>
+        <h1 className='py-3 px-10 bg-pink-400 text-white'>I Love You</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
