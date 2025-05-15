a92a7d48b5f2a274128e53c4d0105b7c7cc2acdf
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-14 23:36:46 -0300

    Feat(carts): Creation of the Carts Migration

---
+++ backend/src/main/resources/db/migration/V5__create_carts_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE carts (
+
+   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   user_id BIGINT UNSIGNED NOT NULL,
+   product_id BIGINT UNSIGNED NOT NULL,
+   quantity INT UNSIGNED NOT NULL,
+   subtotal DECIMAL(10,2) NOT NULL,
+   created_at DATETIME,
+   updated_at DATETIME,
+
+   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+   FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE
+
+);
