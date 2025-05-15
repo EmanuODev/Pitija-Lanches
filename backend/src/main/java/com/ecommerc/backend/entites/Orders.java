8d0968f05d00cceed476afbb2c9045ef577964b6
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-14 23:26:32 -0300

    Feat(orders): Creation of the 'Orders' Entity

---
+++ backend/src/main/java/com/ecommerc/backend/entites/Orders.java
@@ -0,0 +1,85 @@
+package com.ecommerc.backend.entites;
+
+import com.ecommerc.backend.enuns.PaymentMethod;
+import com.ecommerc.backend.enuns.StatusOrders;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+    private float total_price;
+    private StatusOrders status;
+    private PaymentMethod payment_method;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+    @LastModifiedBy
+    private LocalDateTime updated_at;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public float getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(float total_price) {
+        this.total_price = total_price;
+    }
+
+    public PaymentMethod getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(PaymentMethod payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public StatusOrders getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusOrders status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+}
