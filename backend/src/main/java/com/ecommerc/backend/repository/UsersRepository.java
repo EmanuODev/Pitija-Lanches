029c73bb616fc020058e01968119283535ff5cb6
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-14 23:03:40 -0300

    Feat(users): Creation of the 'UsersRepository' Repository for Users

---
+++ backend/src/main/java/com/ecommerc/backend/repository/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerc.backend.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, Long> {
+
+
+
+}
