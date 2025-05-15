d4c822294bb8ab924a36d22248681f5d1ec2e9e7
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-14 23:24:11 -0300

    Feat(users): Creation of the 'users' Migration

--- backend/src/main/resources/db/migration/V1__create_users_table.sql
+++ backend/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users (
 
-       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+       id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(50) NOT NULL,
        email VARCHAR(255) NOT NULL,
        password VARCHAR(255) NOT NULL,
        phone VARCHAR(20) NOT NULL,
        role VARCHAR(6) NOT NULL,
-       createdAt DATETIME,
-       updatedAt DATETIME
+       created_at DATETIME,
+       updated_at DATETIME
 
 );
