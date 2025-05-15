b64c55452d1e45cc77356f2603b867538fb3e7ba
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-14 23:22:57 -0300

    Refactor(users): Changing of the 'Users' Repository

--- backend/src/main/java/com/ecommerc/backend/repository/UsersRepository.java
+++ backend/src/main/java/com/ecommerc/backend/repository/UsersRepository.java
@@ -1,11 +1,11 @@
 package com.ecommerc.backend.repository;
 
-import org.apache.catalina.User;
+import com.ecommerc.backend.entites.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 
 
