03d789302a3434500fbd9be38bbd6f2fdcbe62b0
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-17 23:26:31 -0300

    Feat(orders): Creation of the Create DTO for Order

--- backend/src/main/java/com/ecommerc/backend/dtos/OrderCreateDTO.java
+++ backend/src/main/java/com/ecommerc/backend/dtos/OrderCreateDTO.java
@@ -3,7 +3,7 @@ package com.ecommerc.backend.dtos;
 import com.ecommerc.backend.enuns.PaymentMethod;
 import com.ecommerc.backend.enuns.StatusOrders;
 
-public record CreateOrderDTO(
+public record OrderCreateDTO(
         long user_id,
         float total_price,
         StatusOrders status,
