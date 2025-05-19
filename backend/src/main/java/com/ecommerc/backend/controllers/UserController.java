68ae78379dff93f072643a60df2af81669278da4
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-18 22:14:58 -0300

    Feat(users): Creation of the 'saveUsers' method

--- backend/src/main/java/com/ecommerc/backend/controllers/UserController.java
+++ backend/src/main/java/com/ecommerc/backend/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ecommerc.backend.controllers;
 import com.ecommerc.backend.dtos.UserCreateDTO;
 import com.ecommerc.backend.dtos.UserResponseDTO;
 import com.ecommerc.backend.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    @Autowired
-    UserService userService;
+    private final UserService userService;
 
     @PostMapping()
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserCreateDTO userDTO) {
-        return new ResponseEntity<>(userService.createUser(userDTO), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.saveUser(userDTO), HttpStatus.CREATED);
     }
 
 }
