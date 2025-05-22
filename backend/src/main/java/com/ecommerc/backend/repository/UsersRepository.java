c8532aec71d65eaa90e90fa275af8699356000aa
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-22 11:11:14 -0300

    Feat(users): Created a query to return the quantity of the 'users' Table

--- backend/src/main/java/com/ecommerc/backend/repository/UsersRepository.java
+++ backend/src/main/java/com/ecommerc/backend/repository/UsersRepository.java
@@ -2,9 +2,13 @@ package com.ecommerc.backend.repository;
 
 import com.ecommerc.backend.entites.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    @Query(value = ("SELECT COUNT(*) as count_users FROM users"), nativeQuery = true)
+    long countUsers();
 
 }
