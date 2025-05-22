4b3c1779b98986a74a41c8343c2a4c5d593d1bd1
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-21 23:48:32 -0300

    Chore(order): Removing useless importations of the 'OrderCreateDTO'

--- backend/src/main/java/com/ecommerc/backend/dtos/OrderCreateDTO.java
+++ backend/src/main/java/com/ecommerc/backend/dtos/OrderCreateDTO.java
@@ -1,7 +1,6 @@
 package com.ecommerc.backend.dtos;
 
 import com.ecommerc.backend.enuns.PaymentMethod;
-import com.ecommerc.backend.enuns.StatusOrders;
 
 public record OrderCreateDTO(
         long user_id,
