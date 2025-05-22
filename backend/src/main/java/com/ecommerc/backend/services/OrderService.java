464bbe0986b166f78905a14e574fbecc88cbb13b
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-22 11:31:13 -0300

    Feat(order): Created a method to save order on database

--- backend/src/main/java/com/ecommerc/backend/services/OrderService.java
+++ backend/src/main/java/com/ecommerc/backend/services/OrderService.java
@@ -1,15 +1,33 @@
 package com.ecommerc.backend.services;
 
+import com.ecommerc.backend.dtos.OrderCreateDTO;
+import com.ecommerc.backend.dtos.OrderResponseDTO;
+import com.ecommerc.backend.entites.Orders;
+import com.ecommerc.backend.entites.Users;
+import com.ecommerc.backend.enuns.StatusOrders;
+import com.ecommerc.backend.mappers.OrderMapper;
 import com.ecommerc.backend.repository.OrdersRepository;
+import com.ecommerc.backend.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class OrderService {
 
-    @Autowired
-    OrdersRepository orderRepository;
+    private final OrdersRepository orderRepository;
+    private final UsersRepository userRepository;
 
+    public OrderResponseDTO saveOrder(OrderCreateDTO orderDTO) {
+
+        Users user = userRepository.findById(orderDTO.user_id()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        Orders order = OrderMapper.toEntity(orderDTO, user);
+
+        return OrderMapper.toDTO(orderRepository.save(order));
+
+    }
 
 
 }
