863df50760fabf7037d86668b7c17add98fec4d1
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-22 15:06:06 -0300

    Feat: Component to text of the 'Not Found' page

---
+++ frontend/src/components/h1_not_found.tsx
@@ -0,0 +1,12 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface h1_props extends ComponentProps<'h1'> {
+    children: string
+}
+
+export function H1NotFound ( props: h1_props ) {
+
+    return ( <h1 className={twMerge("font-fascinate text-[12rem] text-slate-700", props.className)}>{props.children}</h1>)
+
+}
