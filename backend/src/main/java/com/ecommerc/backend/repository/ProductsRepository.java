3e1242a040c23b9074e5d9044efeaf3e3154296e
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-05-22 11:02:36 -0300

    Feat(products): Created a query to return the 'price' for 'user_id'

--- backend/src/main/java/com/ecommerc/backend/repository/ProductsRepository.java
+++ backend/src/main/java/com/ecommerc/backend/repository/ProductsRepository.java
@@ -2,8 +2,15 @@ package com.ecommerc.backend.repository;
 
 import com.ecommerc.backend.entites.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
+
+    @Query(value = ("SELECT price FROM products WHERE id = :product_id"), nativeQuery = true)
+    Optional<Float> findPriceById(long product_id);
+
 }
