9806887065642b11f37455115f31146556981656
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-06-06 14:45:03 -0300

    Feat(order_item): The first query was made to show all itens of a order. The second query was made to show the quntity of itens in order

--- backend/src/main/java/com/ecommerc/backend/repository/OrdersItensRepository.java
+++ backend/src/main/java/com/ecommerc/backend/repository/OrdersItensRepository.java
@@ -1,12 +1,20 @@
 package com.ecommerc.backend.repository;
 
+import com.ecommerc.backend.entites.Orders;
 import com.ecommerc.backend.entites.OrdersItens;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrdersItensRepository extends JpaRepository<OrdersItens, Long> {
 
+    @Query(value = ("SELECT * FROM orders_itens WHERE order_id = :order_id"), nativeQuery = true)
+    List<OrdersItens> findOrdersItensByOrder(long order_id);
 
+    @Query(value = ("SELECT COUNT(*) FROM orders_itens WHERE order_id = :order_id"), nativeQuery = true)
+    int countOrdersItensByOrder(long order_id);
 
 }
