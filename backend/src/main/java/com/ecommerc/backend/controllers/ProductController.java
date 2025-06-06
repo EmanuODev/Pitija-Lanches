d0b7f382d4910eda3cc7de474731534ef3a678de
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-06-06 15:10:48 -0300

    Feat(product): I registed a method to connect to the service that show all products by category

--- backend/src/main/java/com/ecommerc/backend/controllers/ProductController.java
+++ backend/src/main/java/com/ecommerc/backend/controllers/ProductController.java
@@ -1,15 +1,13 @@
 package com.ecommerc.backend.controllers;
 
-import com.ecommerc.backend.dtos.ProductCreateDTO;
-import com.ecommerc.backend.dtos.ProductResponseDTO;
+import com.ecommerc.backend.dtos.product.ProductCreateDTO;
+import com.ecommerc.backend.dtos.product.ProductReponseJsonDTO;
+import com.ecommerc.backend.dtos.product.ProductJsonDTO;
 import com.ecommerc.backend.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,10 +17,18 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping()
-    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductCreateDTO productDTO) {
+    public ResponseEntity<ProductReponseJsonDTO> createProduct(@RequestBody ProductCreateDTO productDTO) {
 
-        return new ResponseEntity<>(productService.saveProduct(productDTO), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ProductReponseJsonDTO(productService.saveProduct(productDTO), "Produto criado com sucesso!"), HttpStatus.CREATED);
 
     }
+
+    @GetMapping()
+    public ResponseEntity<ProductJsonDTO> showProductsByCategory(){
+
+        return new ResponseEntity<>(productService.showProductsByCategory(), HttpStatus.OK);
+
+    }
+
 
 }
