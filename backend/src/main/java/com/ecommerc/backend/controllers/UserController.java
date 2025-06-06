83e358560140997f37d0ca280400d2dae35eb515
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-06-06 15:07:12 -0300

    Chore(user): I updated importations because changed the DTO's path

--- backend/src/main/java/com/ecommerc/backend/controllers/UserController.java
+++ backend/src/main/java/com/ecommerc/backend/controllers/UserController.java
@@ -1,10 +1,9 @@
 package com.ecommerc.backend.controllers;
 
-import com.ecommerc.backend.dtos.UserCreateDTO;
-import com.ecommerc.backend.dtos.UserResponseDTO;
+import com.ecommerc.backend.dtos.user.UserCreateDTO;
+import com.ecommerc.backend.dtos.user.UserResponseDTO;
 import com.ecommerc.backend.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
