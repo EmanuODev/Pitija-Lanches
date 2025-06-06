adc1e6a32c1c9f34f0939b3b51ef9c2520c44852
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-06-06 10:51:13 -0300

    Chore: I modified the colors to background

--- frontend/src/pages/not_found.tsx
+++ frontend/src/pages/not_found.tsx
@@ -6,7 +6,7 @@ export function NotFound () {
     return (
         <main className="relative h-screen w-full">
             
-            <div className="absolute inset-0 bg-[url('/not_found.svg')] opacity-80 z-0" />
+            <div className="absolute inset-0 bg-[url('/not_found.svg')] opacity-85 z-10" />
 
             <div className="relative h-screen z-10 w-full flex justify-center items-center pl-32">
                 <div className="flex flex-col pr-7 gap-y-8">
@@ -18,7 +18,7 @@ export function NotFound () {
                             <motion.li animate={ { translateY: [-20, -60, -10] } } transition={ { duration: 3, repeat: Infinity, repeatType: "mirror" } } ><H1NotFound>4</H1NotFound></motion.li>
                         </ul>
                     </div>
-                    <motion.h1 animate={ { translateX: [0, 50, 0] } } transition={ { duration: 4, repeat: Infinity, repeatType: "mirror" } } className="text-center text-[8rem] font-fascinate text-[#f18822]">NOT FOUND</motion.h1>
+                    <motion.h1 animate={ { translateX: [0, 50, 0] } } transition={ { duration: 4, repeat: Infinity, repeatType: "mirror" } } className="text-center text-[8rem] font-Fascinate text-[#D32F2F]">NOT FOUND</motion.h1>
                 </div>
                 <motion.img animate={ {x: [0, 50, 0], scale: [0.9, 1, 0.9]} } transition={{duration: 3, repeat: Infinity, repeatType: "mirror"}} src="../../public/hamburguer.svg" alt="hamburguer" className="h-[55rem]"/>
             </div>
