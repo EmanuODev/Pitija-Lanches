bda763e0c9f0c89cf1a81549b5038fa83a0250a8
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-06-06 10:55:00 -0300

    Refactor: I added style in button and passed the button route

--- frontend/src/components/button_nav.tsx
+++ frontend/src/components/button_nav.tsx
@@ -1,15 +1,20 @@
 import type { ComponentProps, ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface ButtonProps extends ComponentProps<'button'> {
-    children: ReactNode
+
+    children: ReactNode,
+    url: string
+    
 }
 
 
 export function ButtonNav ( props: ButtonProps ) {
 
+    const navigate = useNavigate()
 
     return (
-        <button className="text-[2.2rem] text-[#da2235]">
+        <button {...props} onClick={() => navigate(props.url)} className="text-[2.4rem] text-[#C62828] transition hover:scale-110 rounded-full p-3 hover:bg-[#FFF3E0] hover:bg-opacity-50">
             {props.children}
         </button>
     );
